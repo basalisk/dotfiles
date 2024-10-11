10c689f7e7abbeab7f933e44aeebb468e2bcd23d
James Vandenberg <basalisk@gmail.com>
2024-10-12 08:26:35 +1100

    Adding missing configs

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "Catpuccin Mocha"
+config.font = wezterm.font("VictorMono NF")
+
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = false
+
+return config
