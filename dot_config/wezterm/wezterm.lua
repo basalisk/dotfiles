c0de64db613984806edb3c00468513bf0b804f77
James Vandenberg <basalisk@gmail.com>
2026-01-01 06:32:22 +1100

    I cannot spell catppuccin

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Catpuccin Mocha"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("VictorMono NF")
 
 config.hide_tab_bar_if_only_one_tab = true
